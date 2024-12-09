00918c40ef089af8882c1d7235a3117754641e3d
Katzze94 <germannavasa2003@gmail.com>
2024-12-09 13:57:09 +0100

    Commit_Search_Follow_etc

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -23,12 +23,21 @@ public class EnemyAI : MonoBehaviour
     //Puntos patrulla
     [SerializeField] Transform[] _patrolPoints;
 
+    [SerializeField] Vector2 _patrolAreaSize = new Vector2 (5, 5);   //❤
+
+    [SerializeField] Transform _patrolAreaCenter;
     //Detección
     [SerializeField] float _visionRange = 10;
 
     [SerializeField] float _visionAngle = 120;
     private Vector3 _playerLastPosition;
 
+//Busqueda
+
+float _searchTimer;
+float _searchWaitTime = 15;
+
+float _searchRadius = 10;
     
     void Awake()
     {
@@ -81,14 +90,52 @@ public class EnemyAI : MonoBehaviour
     {
         if(!OnRange())
         {
-            currentState = EnemyState.Patroling;
+            currentState = EnemyState.Searching;
         }
         _AiAgent.destination = _playerTransform.position; 
     }
 
     void Search()
     {
+        if(OnRange())
+        {
+            currentState = EnemyState.Chasing;
 
+        }
+
+        _searchTimer += Time.deltaTime;
+
+        if(_searchTimer < _searchWaitTime)
+        {
+            if(_AiAgent.remainingDistance < 0.5f)
+            {
+                Vector3 randomPoint;
+                if(RandomSearchPoint(_playerLastPosition, _searchRadius, out randomPoint))
+                {
+                    _AiAgent.destination = randomPoint;
+                }
+            }
+        }
+        else
+        {
+            currentState = EnemyState.Patroling;
+            _searchTimer = 0;;;;;;;
+        }
+    }
+
+    bool RandomSearchPoint(Vector3 center, float radius, out Vector3 point)
+    {
+        Vector3 randomPoint = center + Random.insideUnitSphere * radius;
+        NavMeshHit hit;
+        if(NavMesh.SamplePosition(randomPoint, out hit, 4, NavMesh.AllAreas))
+        {
+            point = hit.position;
+            return true ;
+
+        }
+
+        point = Vector3.zero;
+        return false;
     }
 
     bool OnRange()
@@ -143,16 +190,26 @@ public class EnemyAI : MonoBehaviour
 
     void SetRandomPatrolPoint()
     {
-        _AiAgent.destination =_patrolPoints[Random.Range(0, _patrolPoints.Length)].position;
+       // _AiAgent.destination =_patrolPoints[Random.Range(0, _patrolPoints.Length)].position;
+       float RandomX = Random.Range(-_patrolAreaSize.x * 0.5f, _patrolAreaSize.x * 0.5f);
+       float RandomZ = Random.Range(-_patrolAreaSize.y * 0.5f, _patrolAreaSize.y * 0.5f);
+
+       Vector3 randomPoint = new Vector3(RandomX, 0, RandomZ) + _patrolAreaCenter.position;
+
+       _AiAgent.destination = randomPoint;
+
     }
 
     void OnDrawGizmos()
     {
-         foreach (Transform point in _patrolPoints)
+         /*foreach (Transform point in _patrolPoints)
          {
             Gizmos.color = Color.blue;
             Gizmos.DrawWireSphere(point.position, 1f);
-         }
+         }*/
+
+         Gizmos.color = Color.blue;
+         Gizmos.DrawWireCube(_patrolAreaCenter.position, new Vector3(_patrolAreaSize.x, 1, _patrolAreaSize.y));
 
          Gizmos.color = Color.yellow;
          Gizmos.DrawWireSphere(transform.position, _visionRange);
